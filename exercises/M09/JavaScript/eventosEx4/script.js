52217f2ae973c15c04df075e53184fe5d611d4e5
danielqu7e9-hub <daniel.qu.7e9@itb.cat>
2026-01-28 19:20:20 +0100

    Acts event listeners 2-4 Acabats

---
+++ exercises/M09/JavaScript/eventosEx4/script.js
@@ -0,0 +1,45 @@
+let body = document.body;
+
+let caixa = document.getElementById("caixa");
+let contenedorMoviment = document.getElementById("contenedorMoviment");
+
+caixa.addEventListener("mousemove", (event) => {
+    if (event.clientX <= caixa.clientWidth - contenedorMoviment.clientWidth / 2 && event.clientX >= contenedorMoviment.clientWidth / 2 + 9) {
+        contenedorMoviment.style.left = event.clientX - contenedorMoviment.clientWidth / 2 - 9;
+    }
+});
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+        let avionX = contenedorMoviment.offsetLeft + contenedorMoviment.clientWidth / 2 + 11;
+        disparar(avionX - 5);
+    }
+});
+
+document.addEventListener("click", (event) => {
+        let avionX = contenedorMoviment.offsetLeft + contenedorMoviment.clientWidth / 2 + 11;
+        disparar(avionX - 5);
+});
+
+function disparar(clientCoordsX) {
+    let bala = document.createElement("div");
+    bala.style.position = "absolute";
+    bala.style.top = 1050;
+    bala.style.width = 10;
+    bala.style.height = 25;
+    bala.style.backgroundColor = "red";
+    bala.style.left = clientCoordsX;
+
+    caixa.appendChild(bala);
+
+    const idInterval = setInterval(() => {
+        let topActual = parseInt(bala.style.top);
+        if (topActual <= -25) {
+            bala.remove();
+            clearInterval(idInterval);
+        } else {
+            bala.style.top = topActual - 3;
+        }
+    }, 1
+);
+}
